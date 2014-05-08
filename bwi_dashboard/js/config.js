52469907dfae928573ddde252c8d1740facf7e0d
Asher Johnson <Asher@Asher-Johnsons-MacBook-Pro.local>
2014-05-07 20:22:27 -0500

    Added support for custom / default configurations.

--- bwi_dashboard/js/config.js
+++ bwi_dashboard/js/config.js
@@ -1,53 +1,60 @@
-//***************************************************************************
-// EDIT VALUES BELOW
+var connectionConfig = {
+  "localhost" : createConnectionConfig("localhost", 9090, 8080),
+  "hypnotoad" : createConnectionConfig("hypnotoad.csres.utexas.edu", 9090, 8080)
+}
 
-//The address/port of the running ROSBRIDGE Server
-var rosBridgeAddress = 'ws://localhost:9090';
+var topicConfig = {
+  "default" : {
+              VideoTopics: [
+                            '/nav_kinect/rgb/image_raw'
+                           ],
+              SensorTopics: [
+                              createSubscriptionObject('/odom', 'nav_msgs/Odometry', ['twist/twist/linear', 'twist/twist/angular']),
+                              createSubscriptionObject('/clock', 'rosgraph_messages/clock', [])
+                            ]
 
-//The address of the runing Mjpeg Server
-var rosMjpegVideoAddress = 'localhost';
+  },
+  "default2" : {
+              VideoTopics: [
+                           ],
+              SensorTopics: [
+                              createSubscriptionObject('/odom', 'nav_msgs/Odometry', [])
+                            ]
+  }
+}
 
-//The port of the running Mjpeg Server
-var rosMjpegVideoPort = 8080;
-
-//The quality of the streamed video (from 1-100)
-var rosMjpegVideoQuality = 10;
-
-//The topics that will be viewable from video window
-var rosMjpegVideoTopics = ['/nav_kinect/rgb/image_raw'];
-
-//The subscriptions that are shown in the sensor table
-//The format of the function is: createSubscriptionObject(topic name, topic type, topic filters)
-//For instance, to stream just the linear and angular subtopics of odom, you would use:
-//createSubscriptionObject('/odom', 'nav_msgs/Odometry', ['twist/twist/linear','twist/twist/angular'])
-var tableSubscriptions = [
-  createSubscriptionObject('/odom', 'nav_msgs/Odometry', ['twist/twist/linear','twist/twist/angular']),
-  createSubscriptionObject('/clock', 'rosgraph_msgs/Clock', [])
-]
 
 // STOP EDITING VALUES
 //***************************************************************************
 
+ //The server name for the navigation window
+  var navWindowServerName = '/move_base';
 
+  //The topic name for teleop
+  var teleopTopic = '/cmd_vel';
 
+  var rosMjpegServerInfo;
 
+  //The address/port of the running ROSBRIDGE Server
+  var rosBridgeAddress;
 
+  //The address of the runing Mjpeg Server
+  var rosMjpegVideoAddress;
 
-var rosMjpegServerInfo = {
-                           Host : rosMjpegVideoAddress,
-                           Port : rosMjpegVideoPort,
-                           Quality : rosMjpegVideoQuality, //0-100
-                           Height : 480,
-                           Width : 640,
-                           Topics : rosMjpegVideoTopics,
-                           Labels : rosMjpegVideoTopics
-}
+  //The port of the running Mjpeg Server
+  var rosMjpegVideoPort;
 
-//The server name for the navigation window
-var navWindowServerName = '/move_base';
+  //The quality of the streamed video (from 1-100)
+  var rosMjpegVideoQuality = 10;
 
-//The topic name for teleop
-var teleopTopic = '/cmd_vel';
+  //The topics that will be viewable from video window
+  var rosMjpegVideoTopics;
+
+  //The subscriptions that are shown in the sensor table
+  //The format of the function is: createSubscriptionObject(topic name, topic type, topic filters)
+  //For instance, to stream just the linear and angular subtopics of odom, you would use:
+  //createSubscriptionObject('/odom', 'nav_msgs/Odometry', ['twist/twist/linear','twist/twist/angular'])
+  var tableSubscriptions;
 
 //The key values to represent when the following button id is pressed (used for teleop)
 //needed for CustomKeyboardTeleop.js
@@ -58,6 +65,16 @@ var buttonsToHandle = [{id: 'teleopLeftKey', val: 81},
                          {id: 'teleopDownKey', val: 83},
                          {id: 'teleopTurnRightKey', val: 68}];
 
+
+//Used to make the creation of a subscription object easy
+function createConnectionConfig(host, rosbridgeport, mjpegserverport)
+{
+ return {
+         Host: host,
+         RosbridgePort: rosbridgeport,
+         MjpegServerPort: mjpegserverport
+       };
+}
 
 //Used to make the creation of a subscription object easy
 function createSubscriptionObject(topicname, messagetype, subtopicnames)
