f34669eaa642e1caad9d23d0374b1d76dd8d1976
Shiqi Zhang <shiqi.zhang6@gmail.com>
2014-05-15 01:14:59 -0500

    new package created for accepting queries from webpage

---
+++ bwi_coffee_new/src/bwi_coffee_new/planner_coffee_new.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+
+from bwi_coffee import PlannerCoffee
+from bwi_coffee.atom_coffee import AtomCoffee
+from bwi_coffee.action_executor_coffee import ActionExecutorCoffee
+
+class PlannerCoffeeNew(PlannerCoffee):
+
+    def __init__(self): 
+        super(PlannerCoffeeNew, self).__init__()
+
+      
