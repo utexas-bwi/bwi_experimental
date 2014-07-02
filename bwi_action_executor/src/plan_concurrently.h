382d8633b8ba6c744869afe9a4aa3b680db9252b
Jack O'Quin <jack.oquin@gmail.com>
2014-07-02 09:36:13 -0500

    bwi_action_executor: sketch of parallel plan_concurrently, not working yet (#6)

--- bwi_action_executor/src/plan_concurrently.h
+++ bwi_action_executor/src/plan_concurrently.h
@@ -23,10 +23,47 @@
 //
 template <typename T, class F1, class F2>
 std::list<T> plan_concurrently(F1 fn1, F2 fn2) {
+
+#if 1   // serial implementation
+
         std::list<T> result = fn1();
         if (result.empty())
                 result = fn2();
         return result;
+
+#else   // parallel implementation
+
+        // This implementation is rather crude, but I can't find a
+        // standard solution.  It is the simplest I could think of
+        // that might work.
+        //
+        // Creating and destroying two threads on each call is
+        // wasteful, but that cost is tiny compared to forking and
+        // execing a shell and a clasp process to compute each plan.
+
+        template <typename T, class F>
+        class ThreadWrapper {
+        public:
+                std::list<T> result;
+                void tfn(F func) {result = func();}
+                boost::thread thr(tfn);
+                bool finished() {return thr.joinable};
+        }
+        ThreadWrapper<T, F1> t1;
+        ThreadWrapper<T, F2> t2;
+
+        // Polling for thread completion is absurd, but adequate for
+        // our needs.  Feel free to make it better, if necessary.
+        while (true) {
+                // If both finish together, we prefer the second plan.
+                if (t2.finished())
+                        return t2.result;
+                else if (t1.finished())
+                        return t1.result;
+                else
+                        usleep(1000);   // wait a millisecond
+        }
+#endif
 }
 
 #endif // _PLAN_CONCURRENTLY_H_
