0ebe775224b053fb253c8ce6fa58133ad9213aef
Jack O'Quin <jack.oquin@gmail.com>
2014-07-02 08:16:33 -0500

    bwi_action_executor: add timing-dependent plan_concurrently tests (#6)
    Some detect the difference between serial and parallel implementations.
    They are commented out.

--- bwi_action_executor/tests/test_plan_concurrently.cpp
+++ bwi_action_executor/tests/test_plan_concurrently.cpp
@@ -3,40 +3,28 @@
 
 #include <string>
 #include <vector>
+#include <unistd.h>
+#include <boost/bind.hpp>
 #include <gtest/gtest.h>
 #include "../src/plan_concurrently.h"
-
-#if 0  // commented out
-using namespace bwi_actexec;
-
-std::string g_test_action = "test_action";
-std::string g_test_param = "test_parameter";
-std::vector<std::string> g_test_parameters(1, g_test_param);
-
-class TestAction: public Action
-{
-public:
-  int paramNumber() const {return 1;}
-  std::string getName() const {return std::string(g_test_action);}
-  void run() {}
-  bool hasFinished() const {return false;}
-  Action *clone() const {return NULL;}
-private:
-  std::vector<std::string> getParameters() const {return g_test_parameters;}
-};
-
-Action *test_act = new TestAction();
-#endif  // commented out
 
 std::list<int> empty_plan()
 {
   return std::list<int>();
 }
 
-std::list<int> immediate_plan()
+std::list<int> immediate_plan(int result)
 {
   std::list<int> plan;
-  plan.push_back(42);
+  plan.push_back(result);
+  return plan;
+}
+
+std::list<int> delayed_plan(int result, int usecs)
+{
+  usleep(usecs);
+  std::list<int> plan;
+  plan.push_back(result);
   return plan;
 }
 
@@ -48,17 +36,70 @@ TEST(PlanConcurrently, planFailure)
 
 TEST(PlanConcurrently, useFirstPlan)
 {
-  std::list<int> plan = plan_concurrently<int>(immediate_plan, empty_plan);
+  std::list<int> plan = plan_concurrently<int>
+    (boost::bind(immediate_plan, 42),
+     empty_plan);
   EXPECT_FALSE(plan.empty());
   EXPECT_EQ(plan.front(), 42);
 }
 
 TEST(PlanConcurrently, useSecondPlan)
 {
-  std::list<int> plan = plan_concurrently<int>(empty_plan, immediate_plan);
+  std::list<int> plan = plan_concurrently<int>
+    (empty_plan,
+     boost::bind(immediate_plan, 42));
   EXPECT_FALSE(plan.empty());
   EXPECT_EQ(plan.front(), 42);
 }
+
+TEST(PlanConcurrently, useDelayedPlan)
+{
+  std::list<int> plan = plan_concurrently<int>
+    (empty_plan,
+     boost::bind(delayed_plan, 17, 100000));
+  EXPECT_FALSE(plan.empty());
+  EXPECT_EQ(plan.front(), 17);
+}
+
+TEST(PlanConcurrently, useLessDelayedPlan)
+{
+  std::list<int> plan = plan_concurrently<int>
+    (boost::bind(delayed_plan, 7, 50000),
+     boost::bind(delayed_plan, 17, 100000));
+  EXPECT_FALSE(plan.empty());
+  EXPECT_EQ(plan.front(), 7);
+}
+
+TEST(PlanConcurrently, useImmediatePlan)
+{
+  std::list<int> plan = plan_concurrently<int>
+    (boost::bind(immediate_plan, 42),
+     boost::bind(delayed_plan, 17, 100000));
+  EXPECT_FALSE(plan.empty());
+  EXPECT_EQ(plan.front(), 42);
+}
+
+// These tests fail with the serial implementation, they requires
+// actual concurrency.  Save it for the genuine implementation.
+#if 0
+TEST(PlanConcurrently, useSecondImmediatePlan)
+{
+  std::list<int> plan = plan_concurrently<int>
+    (boost::bind(delayed_plan, 17, 100000),
+     boost::bind(immediate_plan, 42));
+  EXPECT_FALSE(plan.empty());
+  EXPECT_EQ(plan.front(), 42);
+}
+
+TEST(PlanConcurrently, useLessDelayedSecondPlan)
+{
+  std::list<int> plan = plan_concurrently<int>
+    (boost::bind(delayed_plan, 17, 100000),
+     boost::bind(delayed_plan, 7, 50000));
+  EXPECT_FALSE(plan.empty());
+  EXPECT_EQ(plan.front(), 7);
+}
+#endif
 
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv)
