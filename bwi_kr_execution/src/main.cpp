52e9a5260d3521bb0734053a3f7a3cd46ff22f5c
Matteo Leonetti <matteo@cs.utexas.edu>
2014-07-10 18:20:56 -0500

    beginning to create the new bwi_kr_execution node

--- bwi_kr_execution/src/main.cpp
+++ bwi_kr_execution/src/main.cpp
@@ -1,9 +1,28 @@
 
 #include "actasp/reasoners/Clingo.h"
 
+#include "actions/ActionFactory.h"
+
+#include <ros/ros.h>
+
+#include <boost/filesystem.hpp>
+
+#include <string>
+
+const int MAX_N = 40;
+const std::string queryDirectory("/tmp/bwi_kr_execution/"); 
+
+using namespace std;
+using namespace bwi_krexec;
+
 int main(int argc, char**argv) {
 	
-	actasp::Clingo clingo(2,"stuff","stuff",std::map<std::string, actasp::Action*>());
+	const string domainDirectory("domain_simulation"); //TODO get this from a parameter
+	ActionFactory::setSimulation(true);
+	
+	
+	boost::filesystem::create_directories(queryDirectory);
+	actasp::Clingo clingo(MAX_N,queryDirectory,domainDirectory,ActionFactory::actions());
 	
 	
 	
