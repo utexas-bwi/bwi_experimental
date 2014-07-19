d0e3cf37b4386150c50215b02f8c7de34dbabd7e
Matteo Leonetti <matteo@cs.utexas.edu>
2014-07-19 14:02:24 -0500

    implementing an actionlib interface for the action execution node

--- bwi_kr_execution/src/action_execution.cpp
+++ bwi_kr_execution/src/action_execution.cpp
@@ -1,6 +1,9 @@
 
 
+#include "msgs_utils.h"
 #include "RemoteReasoner.h"
+
+#include "bwi_kr_execution/ExecutePlanAction.h"
 
 #include "actasp/reasoners/Clingo.h"
 #include "actasp/executors/ReplanningActionExecutor.h"
@@ -10,6 +13,8 @@
 
 #include "actions/ActionFactory.h"
 #include "actions/LogicalNavigation.h"
+
+#include <actionlib/server/simple_action_server.h>
 
 #include <ros/ros.h>
 #include <ros/console.h>
@@ -22,6 +27,50 @@
 using namespace std;
 using namespace bwi_krexec;
 using namespace actasp;
+
+typedef actionlib::SimpleActionServer<bwi_kr_execution::ExecutePlanAction> Server;
+
+
+AspKR *reasoner;
+ActionExecutor *executor;
+
+void executePlan(const bwi_kr_execution::ExecutePlanGoalConstPtr& plan, Server* as) {
+
+  ROS_INFO("Invoked");
+  vector<AspRule> goalRules;
+
+  transform(plan->aspGoal.begin(),plan->aspGoal.end(),back_inserter(goalRules),TranslateRule());
+
+  executor->setGoal(goalRules);
+
+  ros::Rate loop(10);
+
+  while (!executor->goalReached() && !executor->failed() && ros::ok()) {
+
+    if (!as->isPreemptRequested()) {
+      executor->executeActionStep();
+    }
+    else {
+      if(as->isNewGoalAvailable()) {
+        goalRules.clear();
+        const bwi_kr_execution::ExecutePlanGoalConstPtr& newGoal = as->acceptNewGoal();
+        transform(newGoal->aspGoal.begin(),newGoal->aspGoal.end(),back_inserter(goalRules),TranslateRule());
+        executor->setGoal(goalRules);
+      }
+    }
+         loop.sleep();
+  }
+
+
+  if (executor->goalReached()) {
+    ROS_INFO("Execution succeded");
+    as->setSucceeded();
+  }
+  else {
+    ROS_INFO("Execution failed");
+    as->setAborted();
+  }
+}
 
 int main(int argc, char**argv) {
   ros::init(argc, argv, "bwi_action_execution");
@@ -37,33 +86,16 @@ int main(int argc, char**argv) {
 
   ActionFactory::setSimulation(true); //parametrize this
 
-  AspKR *reasoner = new RemoteReasoner();
-  ActionExecutor *executor = new ReplanningActionExecutor(reasoner,reasoner,ActionFactory::actions());
-  
-  vector<AspRule> goalRules(1);
-  
-  goalRules[0].body.push_back(AspFluent("not at(l3_516,I)"));
-  
-  executor->setGoal(goalRules);
-  
-  ros::Rate loop(10);
-  
-  while (ros::ok() && !executor->goalReached() && !executor->failed()) {
-    
-    executor->executeActionStep();
+  reasoner = new RemoteReasoner();
+  executor = new ReplanningActionExecutor(reasoner,reasoner,ActionFactory::actions());
 
-    ros::spinOnce();
+  Server server(n, "execute_plan", boost::bind(&executePlan, _1, &server), false);
+  server.start();
 
-    loop.sleep();
-  }
-  
-  if(executor->failed()) {
-    ROS_INFO("Execution failed");
-  }
-  if(executor->goalReached()) {
-    ROS_INFO("Execution succeded");
-  }
-  
+  ros::spin();
+
+  server.shutdown();
+
   delete executor;
   delete reasoner;
 
