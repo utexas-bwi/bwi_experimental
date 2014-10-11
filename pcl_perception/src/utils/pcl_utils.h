af9be7a776d1a192f1750f9e1e12673542861993
Jivko Sinapov <jsinapov@cs.utexas.edu>
2014-10-11 13:58:49 -0500

    Package for people detection using Point Cloud Library.

---
+++ pcl_perception/src/utils/pcl_utils.h
@@ -0,0 +1,22 @@
+// PCL specific includes
+#include <pcl/conversions.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/console/parse.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>    
+#include <pcl/io/openni_grabber.h>
+#include <pcl/sample_consensus/sac_model_plane.h>
+#include <pcl/people/ground_based_people_detection_app.h>
+#include <pcl/common/time.h>
+
+#include <signal.h> 
+#include <vector>
+#include <string.h>
+
+
+namespace pcl_utils {
+
+
+
+}
