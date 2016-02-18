94901d4a3d531c7343ea697c7ed2cdfd51c4fdca
Pato <plankenau@gmail.com>
2016-02-18 13:53:28 -0600

    added ability to view the costmap

--- bwi_costmaps/scripts/view_bag.py
+++ bwi_costmaps/scripts/view_bag.py
@@ -27,6 +27,22 @@ def view(costmap):
     print(len(data))
     
     # TODO: will need to reconstruct the matrix from the 1D array
+    matrix = np.array(data).reshape((height,width))
+
+    fig = plt.figure(figsize=(6,3.2))
+
+    ax = fig.add_subplot(111)
+    ax.set_title("color")
+    plt.imshow(matrix)
+    ax.set_aspect('equal')
+
+    cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+    cax.get_xaxis().set_visible(False)
+    cax.get_yaxis().set_visible(False)
+    cax.patch.set_alpha(0)
+    cax.set_frame_on(False)
+    plt.colorbar(orientation='vertical')
+    plt.show()
 
 def get_costmaps():
     # Init ros node
