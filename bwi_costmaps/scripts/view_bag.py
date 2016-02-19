eb06662ecb5f21138f5dcb7393209a362d621335
Pato <plankenau@gmail.com>
2016-02-19 12:44:26 -0600

    reorganized display to it looks better

--- bwi_costmaps/scripts/view_bag.py
+++ bwi_costmaps/scripts/view_bag.py
@@ -29,19 +29,20 @@ def view(costmap):
     # TODO: will need to reconstruct the matrix from the 1D array
     matrix = np.array(data).reshape((height,width))
 
-    fig = plt.figure(figsize=(6,3.2))
+    fig = plt.figure(figsize=(16,6))
 
-    ax = fig.add_subplot(111)
-    ax.set_title("color")
-    plt.imshow(matrix)
+    ax = fig.add_subplot(1,1,1)
+    ax.set_title("Costmap Heatmap")
     ax.set_aspect('equal')
+    plt.imshow(matrix)
 
-    cax = fig.add_axes([0.12, 0.1, 0.78, 0.8])
+    cax = fig.add_axes([0.1, 0.1, 0.96, 0.8])
     cax.get_xaxis().set_visible(False)
     cax.get_yaxis().set_visible(False)
     cax.patch.set_alpha(0)
     cax.set_frame_on(False)
-    plt.colorbar(orientation='vertical')
+    plt.colorbar(orientation='vertical',drawedges=False)
+
     plt.show()
 
 def get_costmaps():
