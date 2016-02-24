3a910b25b8044a245f0962f5f36be964b4b53202
Pato <plankenau@gmail.com>
2016-02-24 13:49:22 -0600

    added beginning of make costmaps script

---
+++ bwi_costmaps/scripts/make_costmaps.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+"""
+Author: Pato Lankenau (plankenau@gmail.com)
+License: BSD
+"""
+from __future__ import print_function
+from nav_msgs.msg import OccupancyGrid
+import rospy
+import sys
+import rosbag
+import yaml
+import pprint
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+def get_costmaps():
+    # Init ros node
+    rospy.init_node('bag_viewer')
+
+    # Get a pretty printer
+    pp = pprint.PrettyPrinter()
+
+    # Get the bag to view
+    whichBag = rospy.get_param("~bag", None)
+
+    # Ensure the bag was specified
+    if whichBag == None:
+        print("No bag specified", file=sys.stderr)
+        return 9
+    rospy.loginfo("Bag to view: " + whichBag)
+
+    # Load the bag
+    bag = rosbag.Bag(whichBag)
+
+    # Get bag information
+    info = yaml.load(bag._get_yaml_info())
+    pp.pprint(info)
+
+    # Read the bag
+    for topic, msg, time in bag.read_messages():
+        print(topic)
+        #view(msg)
+
+    # Close the bag
+    bag.close()
+
+    return 0
+
+if __name__ == '__main__':
+    try:
+        get_costmaps()
+    except rospy.ROSInterruptException:
+        pass
