a4375f1f00cc9336c40cec5ee44e0c0f7ca085e1
Pato <plankenau@gmail.com>
2016-02-24 13:55:17 -0600

    wrote method to extract costmap and turn into matrix

--- bwi_costmaps/scripts/make_costmaps.py
+++ bwi_costmaps/scripts/make_costmaps.py
@@ -14,6 +14,25 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+"""
+Return a numpy matrix containing the costmap
+"""
+def constructCostmap(costmap_msg):
+    hdr  = costmap.header
+    info = costmap.info
+    data = costmap.data
+
+    width  = info.width
+    height = info.height
+    
+    return np.array(data).reshape((height,width))
+
+"""
+Takes a costmap and a update message and applies the patch
+"""
+def applyPatch(costmap, updateMsg):
+    return
+
 def get_costmaps():
     # Init ros node
     rospy.init_node('bag_viewer')
@@ -38,9 +57,17 @@ def get_costmaps():
     pp.pprint(info)
 
     # Read the bag
+    costmap = None
     for topic, msg, time in bag.read_messages():
-        print(topic)
-        #view(msg)
+        if topic == "/move_base/global_costmap/costmap":
+            costmap = constructCostmap
+        else:
+            if costmap == None:
+                rospy.logerror("Found a costmap update without first finding a costmap")
+                return 8
+            else:
+                applyPatch(costmap, msg)
+                continue
 
     # Close the bag
     bag.close()
